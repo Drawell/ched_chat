902f17d4f2ded8d73a7651e4213e5bfeeeed478f
Drawell <ivanevgnikonov@gmail.com>
2023-01-11 17:33:10 +0300

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [hello, setHello] = useState('')
+
+  useEffect(() => {
+    const request = async () => {
+      const response = await fetch('api/hello')
+      const json = await response.json()
+
+      setHello(json.message)
+    }
+
+    request()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h3>{hello}</h3>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
