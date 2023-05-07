03b71ca84da2bdd853b811a0a9e3f3eeec5053f3
Drawell <ivanevgnikonov@gmail.com>
2023-05-07 18:40:55 +0300

    Upgrade code generator

--- api/codegen.py
+++ api/codegen.py
@@ -1,48 +1,30 @@
-import re
+
+from dataclasses import dataclass
 
 
-sql_types_map = {
-    'integer': 'Integer',
-    'character varying': 'String',
-    'text': 'Text',
-    'boolean': 'Boolean',
-    'timestamp without time zone': 'DateTime',
-
-}
-
-
-def _handle_default(default: str):
-    if default == 'True':
-        return ', default=True'
-    elif default == 'CURRENT_TIMESTAMP':
-        return ', default=func.now()'
-    elif len(default) > 7 and default[:7] == 'nextval':
-        return ', primary_key=True, autoincrement=True'
-    else:
-        return ''
+@dataclass
+class CommonAttribute:
+    name: str
+    attr_type: str
+    default_value: str
+    nullable: bool
+    size: int
 
 
-def _generate_code_for_column(sql_column_str: str):
-    column_attributes = sql_column_str.split('\t')
-    if len(column_attributes) < 4:
-        return ''
+def parse_sql_columns(sql_columns: str) -> tuple[str, list[CommonAttribute]]:
+    sql_columns = sql_columns.split('\n')
+    table_name = get_table_name(sql_columns)
 
-    column_name = column_attributes[1].replace('"', '')
-    default_value = column_attributes[2].replace('"', '')
-    nullable = column_attributes[3].replace('"', '')
-    type = column_attributes[4].replace('"', '')
-    size = None
-    if len(column_attributes) >= 5:
-        size = column_attributes[5]
-    size_str = f'({size})' if size else ''
+    attrs = []
+    for column in sql_columns:
+        attr = parse_column(column)
+        if attr:
+            attrs.append(attr)
 
-    default_str = _handle_default(default_value)
-    nullable_str = ', nullable=False' if nullable == 'NO' else ''
-
-    return f'{column_name} = Column({sql_types_map.get(type)}{size_str}{default_str}{nullable_str})'
+    return table_name, attrs
 
 
-def _get_table_name(columns: list):
+def get_table_name(columns: list):
     if len(columns) < 2:
         return ''
 
@@ -52,32 +34,125 @@ def _get_table_name(columns: list):
     return table_name
 
 
-def _get_class_name(table_name: str):
-    if len(table_name) < 3:
-        return 'class TMP(Base)'
-    else:
-        class_name_snake = table_name[3:]
-        words = class_name_snake.split('_')
-        class_name = ''.join(x.title() for x in words)
-        return class_name
+def parse_column(sql_column: str) -> CommonAttribute:
+    sql_types_map = {
+        'integer': 'int',
+        'character varying': 'str',
+        'text': 'str',
+        'boolean': 'bool',
+        'timestamp without time zone': 'datetime',
+    }
+
+    column_attributes = sql_column.split('\t')
+    if len(column_attributes) < 4:
+        return None
+
+    name = column_attributes[1].replace('"', '')
+    default_value = column_attributes[2].replace('"', '')
+
+    nullable = column_attributes[3].replace('"', '')
+    nullable = nullable != 'NO'
+
+    attr_type = column_attributes[4].replace('"', '')
+    attr_type = sql_types_map.get(attr_type.strip())
+
+    size = None
+    if len(column_attributes) >= 6:
+        size = int(column_attributes[5])
+
+    attr = CommonAttribute(name, attr_type, default_value, nullable, size)
+
+    return attr
 
 
-def generate_sqlalchemy_model(sql_columns_str: str):
-    columns = sql_columns_str.split('\n')
+class CodeGen:
+    def __init__(self, table_name: str, attrs: list[CommonAttribute], indent: str = '    '):
+        self.table_name = table_name
+        self.attrs = attrs
+        self.indent = indent
 
-    table_name = _get_table_name(columns)
-    class_name = _get_class_name(table_name)
-    result = '\n'
-    result += f'class {class_name}(Base):\n'
-    result += f"    __tablename__ = '{table_name}'\n"
-    for item in columns:
-        result += f'    {_generate_code_for_column(item)}\n'
+    def generate_code(self):
+        result = ''
+        result += self.generate_name()
+        result += self.indent + self.generate_post_name()
+        for attr in self.attrs:
+            result += self.indent + self.generate_attr(attr)
+        result += self.indent + self.generate_post_attrs()
+        return result
 
-    return result
+    def generate_name(self):
+        return ''
+
+    def generate_post_name(self):
+        return ''
+
+    def generate_attr(self, attr: CommonAttribute):
+        return ''
+
+    def generate_post_attrs(self):
+        return ''
 
 
-sql_columns = '''
-'''
+class SQLALchemyCodeGen(CodeGen):
+    types_map = {
+        'int': 'Integer',
+        'str': 'String',
+        'bool': 'Boolean',
+        'datetime': 'DateTime',
+    }
+
+    def __init__(self, table_name: str, attrs: list[CommonAttribute]):
+        super().__init__(table_name, attrs, indent='    ')
+
+    def generate_name(self):
+        if len(self.table_name) < 3:
+            class_name = 'class TMP(Base)\n'
+        else:
+            class_name_snake = self.table_name[3:]
+            words = class_name_snake.split('_')
+            class_name = ''.join(x.title() for x in words)
+
+        return f'class {class_name}(Base):\n'
+
+    def generate_post_name(self):
+        return f"__tablename__ = '{self.table_name}'\n\n"
+
+    def generate_attr(self, attr: CommonAttribute):
+        type_str = SQLALchemyCodeGen.types_map.get(attr.attr_type)
+        default_str = self._handle_default(attr.default_value)
+        nullable_str = ', nullable=False' if not attr.nullable else ''
+        size_str = f'({attr.size})' if attr.size else ''
+
+        return f'{attr.name} = Column({type_str}{size_str}{default_str}{nullable_str})\n'
+
+    def _handle_default(self, default: str):
+        if default == 'True':
+            return ', default=True'
+        elif default == 'CURRENT_TIMESTAMP':
+            return ', default=func.now()'
+        elif len(default) > 7 and default[:7] == 'nextval':
+            return ', primary_key=True, autoincrement=True'
+        else:
+            return ''
 
 
-print(generate_sqlalchemy_model(sql_columns))
+class PydanticCodeGen(CodeGen):
+
+    def __init__(self, table_name: str, attrs: list[CommonAttribute]):
+        super().__init__(table_name, attrs, indent='    ')
+
+    def generate_name(self):
+        if len(self.table_name) < 3:
+            class_name = 'class TMP(BaseModel)\n'
+        else:
+            class_name_snake = self.table_name[3:]
+            words = class_name_snake.split('_')
+            class_name = ''.join(x.title() for x in words)
+
+        return f'class {class_name}(BaseModel):\n'
+
+    def generate_post_name(self):
+        return f'class Config:\n{self.indent * 2}orm_mode = True\n\n'
+
+    def generate_attr(self, attr: CommonAttribute):
+        return f'{attr.name}: {attr.attr_type}\n'
