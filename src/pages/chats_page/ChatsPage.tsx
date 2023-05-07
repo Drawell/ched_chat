e325e84fd057fe461df3f8b42798fb9b8609a0a6
Drawell <ivanevgnikonov@gmail.com>
2023-05-07 20:16:30 +0300

    Simple chats list

--- src/pages/chats_page/ChatsPage.tsx
+++ src/pages/chats_page/ChatsPage.tsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from 'auth/AuthProvider'
+import ChatsList from 'widgets/chats_list/ChatsList'
 
 const ChatsPage: React.FC = () => {
   const { curChed, signout } = useAuth()
+  const [chatId, setChatId] = useState<number | null>(null)
 
   const handleLogout = () => {
     signout()
   }
 
+  const handleSelectChat = (chatId: number) => {
+    setChatId(chatId)
+  }
+
   return (
     <div>
       {curChed ? <h5>Curent chad is {curChed?.name}</h5> : <h5>Need to login</h5>}
-      <p>TODO: chats page</p>
       <form>
         <button onClick={handleLogout}>Выход</button>
       </form>
+
+      <ChatsList selectedId={chatId} onSelectChat={handleSelectChat} />
+
+      {chatId ? <div>Current chat id: {chatId}</div> : <div>Select chat</div>}
     </div>
   )
 }
